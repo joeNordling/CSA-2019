b070d1994112c4213a2affc3337591cca472a8aa
headshotninja21 <36750741+headshotninja21@users.noreply.github.com>
2019-10-08 14:06:33 -0500

    1.2.1 Part II

--- MediaLib/MediaLib.java
+++ MediaLib/MediaLib.java
@@ -4,9 +4,10 @@ public class MediaLib
     {
         Song song2 = new Song(); 
         song2.setTitle("this is the title");
-        String t = song2.getTitle();
+        song2.setRating(10);
+        String t = song2.getTitle() + " | " + song2.getRating();
         
-        System.out.println("Welcome to your media library");
+        System.out.println(t);
         
         MediaFile.writeString(t);
         MediaFile.saveAndClose();
