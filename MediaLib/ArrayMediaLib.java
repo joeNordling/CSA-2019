c94449cf76c9039bfbe26db8d253134e72f35fc0
headshotninja21 <36750741+headshotninja21@users.noreply.github.com>
2019-10-10 14:08:31 -0500

    1.2.2 Part III

--- MediaLib/ArrayMediaLib.java
+++ MediaLib/ArrayMediaLib.java
@@ -1,7 +1,7 @@
 public class ArrayMediaLib
 {
     public ArrayMediaLib()
-    {
+    {      
         String[] sharingFriends = 
         {
             new String("Friend1"),
@@ -10,10 +10,24 @@ public class ArrayMediaLib
             new String("Friend4"),
             new String("Friend5")
         };
-        
         for (int i = 0; i < 5; i++)
         {
             System.out.println(sharingFriends[i]);
         }
+        
+        int[] daysBtwnPurchase = {2,5,1,2,4,2,1,3};
+        System.out.println(totalDay(daysBtwnPurchase));
+    }
+    public double totalDay(int[] DL)
+    {
+        double total = 0;
+        double dayC = 0;
+        
+        for (double day : DL)
+        {
+            total = total + day;
+            dayC++;
+        }
+        return(total/dayC);
     }
 }
