40650fd14c0d13b51d2bd5f9ac3606fbe2d9b697
ConnorRush <56092613+ConnorRush@users.noreply.github.com>
2019-10-15 15:21:35 -0500

    ArrayMediaLib1.2.2

---
+++ ArrayMediaLib/MainMediaLib.java
@@ -0,0 +1,12 @@
+public class MainMediaLib
+{
+    String[] sharingFriends = 
+    {
+        new String("Connor"), 
+        new String("George"), 
+        new String("Joe"), 
+        new String("Kyle"), 
+        new String("Charlie")    
+    };
+        for (int i = 
+}
