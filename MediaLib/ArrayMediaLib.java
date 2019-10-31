f09bc8ff492e5b99d023b10bd7228e5bce45ff7d
headshotninja21 <36750741+headshotninja21@users.noreply.github.com>
2019-10-31 14:11:11 -0500

    1.2.2

--- MediaLib/ArrayMediaLib.java
+++ MediaLib/ArrayMediaLib.java
@@ -17,6 +17,7 @@ public class ArrayMediaLib
         
         double[] daysBtwnPurchase = {2,5,1,2,4,2,1,3};
         System.out.println(Avg(daysBtwnPurchase));*/
+<<<<<<< Updated upstream
         song[] topTensongs =
         {
             new song("The Twist"), 
@@ -33,6 +34,37 @@ public class ArrayMediaLib
         for (song s: topTensongs)
         {
         System.out.println(s.getTitle());
+=======
+        String[] songNames = 
+        {
+            new String("The Twist"),
+            new String("Smoooth"),
+            new String("Mack the Knife"),
+            new String(""),
+            new String(""),
+            new String(""),
+            new String(""),
+            new String(""),
+            new String(""),
+            new String("")
+        };
+        Song[] topTenSongs =
+        {
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song(),
+            new Song()
+        };
+        for (int i = 0; i < 10; i++)
+        {
+            
+>>>>>>> Stashed changes
         }
     }
     public double Avg(double[] DL)
