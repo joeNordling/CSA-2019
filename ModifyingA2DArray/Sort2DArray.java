1d2016f151726e8420183b3e733483d4d8c6fd0b
Joe Nordling <joenordling@icloud.com>
2019-11-25 22:52:20 -0600

    Sorting 2d array finished

--- ModifyingA2DArray/Sort2DArray.java
+++ ModifyingA2DArray/Sort2DArray.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class Sort2DArray
-{
-    
-    public static void Sort2DArray()
-    {
+{   
+    public static void Sort2DArray(){
         int[][] D2Array = {{1,5,4,8,7,3},{2,4,3,5,7,6},{2,4,3,3,6,0},{9,8,9,9,4,1}};
-        sort2DArray(D2Array);
-        
+        int[][] SortedArray = modify(D2Array);
+        System.out.println(Arrays.deepToString(SortedArray));
     }
 
-    public static int[][] sort2DArray(int[][] StartingArray){
-        for(int i = 0; i < StartingArray.length)
+    public static int[][] modify(int[][] StartingArray){
+        for(int i = 0; i < StartingArray.length; i++){
+            if(i % 2 != 0){
+                for(int j = 0; j < StartingArray[i].length; j++){
+                    StartingArray[i][j] = StartingArray[i-1][j];
+                }
+            }
+        }
         return StartingArray;
     }
 }
